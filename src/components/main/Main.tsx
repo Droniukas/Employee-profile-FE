2b1321992f72867266f168e70d844c37e0487c45
nerijusgrigas.lt <nerijusgrigas.lt@gmail.com>
2023-03-21 21:13:33 +0200

    Implement main, header and footer components

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.scss';
+import FindEmployee from './findEmployee/FindEmployee';
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <FindEmployee/>
+        </div>
+    );
+};
+
+export default Main;
