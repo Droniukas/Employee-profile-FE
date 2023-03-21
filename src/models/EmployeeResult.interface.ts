d294c7b4296696cdf73e8555e27d76f8d4ee6f1c
nerijusgrigas.lt <nerijusgrigas.lt@gmail.com>
2023-03-21 21:14:59 +0200

    Implement FindEmployees components and model

---
+++ src/models/EmployeeResult.interface.ts
@@ -0,0 +1,12 @@
+interface EmployeeResult {
+    id: number,
+    name: string,
+    surname: string,
+    middle_name: string,
+    image: string,
+    hiring_date: string,
+    exit_date: string,
+    title_id: string,
+}
+
+export default EmployeeResult;
