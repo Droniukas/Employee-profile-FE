8f5c9e1a4d15e791a3ba3d3649cf2b6a2c1e4d5e
nerijusgrigas.lt <nerijusgrigas.lt@gmail.com>
2023-03-21 21:15:23 +0200

    Implement debounced state

---
+++ src/hooks/useDebouncedState.tsx
@@ -0,0 +1,22 @@
+import {useState, useEffect} from 'react';
+
+type UseDebouncedStateReturnValue = [string, (value: string) => void];
+
+const useDebouncedState = (initialValue: string, debouncedTime: number): UseDebouncedStateReturnValue => {
+    const [value, setValue] = useState(initialValue);
+    const [debouncedValue, setDebouncedValue] = useState(initialValue);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setDebouncedValue(value);
+        }, debouncedTime);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [debouncedTime, value]);
+
+    return [debouncedValue, setValue];
+}
+
+export default useDebouncedState;
