08dea9f7d2feb199a777268aaa8427e739e21156
Simonas Eicinas <simonas.eicinas@gmail.com>
2023-03-22 00:35:19 +0200

    tabs added with skills tab integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import SkillsTabList from './components/skills-tab/SkillsTabList'
-import { CssBaseline, Box } from '@mui/material'
+import { CssBaseline, Box, Tabs, Tab } from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { checkboxClasses } from '@mui/material/Checkbox'
+import Typography from '@mui/material/Typography';
+
 const theme = createTheme({
   typography: {
     fontFamily: ['Inter', 'sans-serif'].join(','),
@@ -50,13 +52,77 @@ const theme = createTheme({
   },
 })
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+
+
+
+
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '70vw', margin: '150px 250px 0px'}}>
+          <Tabs value={value} onChange={handleChange} indicatorColor="secondary" aria-label="secondary">
+            <Tab label="Skills"           {...a11yProps(0)} />
+            <Tab label="Achievements"     {...a11yProps(1)} />
+            <Tab label="My projects"      {...a11yProps(2)} />
+            <Tab label="Search"           {...a11yProps(3)} />
+            <Tab label="Project profiles" {...a11yProps(4)} />
+          </Tabs>
+        </Box>
+
         <Box display='flex' justifyContent='center' alignItems='center'>
+        <TabPanel value={value} index={0}>
           <SkillsTabList />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Achievements
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          My projects
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Search
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          Project profiles
+        </TabPanel>
         </Box>
       </ThemeProvider>
     </>
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Main = () => {
+
+
+
+
+    
+  return (
+
+
+
+    
+    <div>Main</div>
+  )
+}
+
+export default Main;
