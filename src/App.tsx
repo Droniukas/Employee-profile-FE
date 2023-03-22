378ce09393348f3151fe11f2dd6ebebc5b25400d
ArnoldasR <arokon.arnoldas.reklys@gmail.com>
2023-03-22 12:34:20 +0200

    EP-19-login-page added .data/theme folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,21 @@
 import './App.css'
 import Login from './components/login/Login'
-import { createMuiTheme, ThemeProvider } from '@mui/material/styles';
+import {ThemeProvider} from '@mui/material/styles';
+import Theme from './data/Theme';
 import Grid from '@mui/material/Grid';
 import Footer from './components/login/Footer';
 
-const theme = createMuiTheme ({
-  typography: {
-    fontFamily: ['Inter', 'sans-serif'
-  ].join(','),
-  fontSize: 14,
-  button: { 
-    fontWeight: 600,
-    textTransform: 'none',
-  },
-
-},
-
-  palette: {
-      primary : {
-        main: '#000048'
-      },
-      secondary : {
-        main: '#78ECE8'
-      }
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: '100px',  
-        },
-      },
-    }, 
-  },
-
-});
-
 function App() {
-  return (
-    
-    <ThemeProvider theme={theme}>
+  return ( 
+    <ThemeProvider theme={Theme}>
     <Grid
   container sx={{
   spacing:0,
   direction:'column',
   alignItems:'center',
   justifyContent:'center'
-
 }}
 >
-
     <Login></Login>
     </Grid>
     <Footer></Footer>
---
+++ src/data/Theme.tsx
@@ -0,0 +1,35 @@
+import { createMuiTheme, ThemeProvider } from '@mui/material/styles';
+
+const Theme = createMuiTheme ({
+    typography: {
+      fontFamily: ['Inter', 'sans-serif'
+    ].join(','),
+    fontSize: 14,
+    button: { 
+      fontWeight: 600,
+      textTransform: 'none',
+    },
+  
+  },
+  
+    palette: {
+        primary : {
+          main: '#000048'
+        },
+        secondary : {
+          main: '#78ECE8'
+        }
+    },
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            borderRadius: '100px',  
+          },
+        },
+      }, 
+    },
+  
+  });
+
+  export default Theme;
