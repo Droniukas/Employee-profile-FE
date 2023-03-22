7e99e04cdcde9d7464ff39db5459130424efb020
nerijusgrigas.lt <nerijusgrigas.lt@gmail.com>
2023-03-22 18:11:53 +0200

    Implement result list

--- src/components/main/findEmployee/FindEmployeeResults.tsx
+++ src/components/main/findEmployee/FindEmployeeResults.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import EmployeeResult from '../../../models/EmployeeResult.interface';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
 
 type Props = {
     results: EmployeeResult[];
@@ -7,30 +14,54 @@ type Props = {
 
 const FindEmployeeResults: React.FC<Props> = ({results}) => {
     if (!results.length) return null;
+    let fullName;
 
     return (
-        <table className="ui celled table">
-            <thead>
-            <tr>
-                <th>photo</th>
-                <th>name</th>
-                <th>middle name</th>
-                <th>surname</th>
-                <th>title</th>
-            </tr>
-            </thead>
-            <tbody>
-            {results.map((result) => (
-                <tr key={result.id}>
-                    <td data-label='photo'>{result.image}</td>
-                    <td data-label='name'>{result.name}</td>
-                    <td data-label='middle_name'>{result.middle_name}</td>
-                    <td data-label='surname'>{result.surname}</td>
-                    <td data-label='title'>{result.title_id}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+        <List
+            sx={{
+                width: '100%'
+            }}>
+            {results
+                .sort((a, b) => a.name > b.name ? 1 : -1)
+                .map((result) => (
+                    <>
+                        <Box
+                            sx={{
+                                display: {
+                                    xl: 'none',
+                                    xs: 'none'
+                                }
+                            }}>
+                            {result.middle_name
+                                ? fullName = `${result.name} ${result.middle_name} ${result.surname}`
+                                : fullName = `${result.name} ${result.surname}`
+                            }
+                        </Box>
+                        <ListItem alignItems='flex-start'>
+                            <ListItemAvatar>
+                                <Avatar
+                                    src='/static/images/avatar/1.jpg'
+                                />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={fullName}
+                                secondary={
+                                    <React.Fragment>
+                                        {result.title_id}
+                                    </React.Fragment>
+                                }
+                                sx={{
+                                    color: '#000048'
+                                }}
+                            />
+                        </ListItem>
+                        <Divider
+                            variant='fullWidth'
+                            component='li'
+                        />
+                    </>
+                ))}
+        </List>
     );
 };
 
