d44a0af396b4c40e94d30ab14b750056f1c42bdd
Simonas Eicinas <simonas.eicinas@gmail.com>
2023-03-28 09:52:59 +0300

    trying to merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import React from 'react'
-import SkillsTabList from './components/skills-tab/SkillsTabList'
-import { CssBaseline, Box, Tabs, Tab } from '@mui/material'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { checkboxClasses } from '@mui/material/Checkbox'
-import Typography from '@mui/material/Typography';
 import Header from './components/header/Header'
 import Main from './components/main/Main'
 import Footer from './components/footer/Footer'
