c6f4524a17006a4a924b1f4312182a9b794e162a
Simonas Eicinas <simonas.eicinas@gmail.com>
2023-03-28 09:47:12 +0300

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import SkillsTabList from './components/skills-tab/SkillsTabList'
+import { CssBaseline, Box, Tabs, Tab } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { checkboxClasses } from '@mui/material/Checkbox'
+import Typography from '@mui/material/Typography';
+import Header from './components/header/Header'
+import Main from './components/main/Main'
+import Footer from './components/footer/Footer'
+import './App.scss'
+import { yellow } from '@mui/material/colors'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <body>
+        <img src='https://logosandtypes.com/wp-content/uploads/2022/03/Cognizant.png' alt='' className='img'></img>
+        <main>
+          <Header />
+          <Main />
+        </main>
+
+        <footer>
+           <Footer />
+        </footer>
+    </body>
+   
+    
+    </>
   )
 }
 
