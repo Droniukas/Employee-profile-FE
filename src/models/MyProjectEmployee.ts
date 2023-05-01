01cd77672cc135b4fc2af795b9e3183b5346188b
ArnoldasR <arokon.arnoldas.reklys@gmail.com>
2023-05-01 18:52:11 +0300

    New commit fresh commits

---
+++ src/models/MyProjectEmployee.ts
@@ -0,0 +1,8 @@
+interface MyProjectEmployee {
+  id: number;
+  projectId: number;
+  employeeId: number;
+  responsibilities: string;
+}
+
+export default MyProjectEmployee;
--- src/services/projects.service.ts
+++ src/services/projects.service.ts
@@ -1,6 +1,7 @@
 import { FormikValues } from 'formik';
 
 import { ProjectStatus } from '../components/enums/ProjectStatus';
+import MyProjectEmployee from '../models/MyProjectEmployee';
 import Project from '../models/Project.interface';
 import axios from './axios';
 
@@ -45,5 +46,70 @@ export class ProjectsService {
         project.status = ProjectStatus.FINISHED;
       }
     }
+  }
+  public async getMyProjects() {
+    const response = await axios.get('/project/projectsByEmployeeId/' + `${process.env.REACT_APP_TEMP_USER_ID}`);
+    const myProjectEmployee = response.data.map((myProjectEmployee: MyProjectEmployee) => {
+      return myProjectEmployee;
+    });
+    const projectIds = myProjectEmployee.map((myProjectEmployee: MyProjectEmployee) => myProjectEmployee.projectId);
+    console.log(projectIds);
+    console.log('data' + myProjectEmployee);
+
+    const projectRequests = projectIds.map((projectId: string) => {
+      return axios.get(`/project/get/${projectId}`);
+    });
+
+    const projectResponses = await Promise.all(projectRequests);
+    const projects = projectResponses.map((response: any) => response.data);
+    projects.forEach((project: Project) => {
+      this.mapProjectStatus(project);
+    });
+
+    return projects;
+  }
+  // public async addTitleToProjectEmployee(projectEmployee: ProjectEmployee) {
+  //   const { projectId, employeeId, titleId } = projectEmployee;
+  //   const data = {
+  //     projectId: projectId,
+  //     employeeId: employeeId,
+  //     titleId: titleId,
+  //   };
+  //   const response = await axios.post('project/projects-employees/', data);
+  //   return response.data;
+  // }
+  public async getResponsibilitiesFromProjectEmployee(projectId: number) {
+    const response = await axios.get(`project/responsibilities/ ${projectId}/${process.env.REACT_APP_TEMP_USER_ID}`);
+    const plainText = response.data;
+    const responsibilities = plainText.split('\n').filter((line: string) => line.trim() !== '');
+    return responsibilities;
+
+    console.log('data' + response.data);
+    return response.data;
+  }
+  // public async getResponsibilitiesFromProjectEmployee() {
+  //   const projectEmployee = response.data.map((projectEmployee: ProjectEmployee) => {
+  //     return projectEmployee;
+  //   });
+
+  public async addResponsibilitiesToProjectEmployee(myProjectEmployee: MyProjectEmployee) {
+    const { projectId, employeeId, responsibilities } = myProjectEmployee;
+    const data = {
+      projectId: projectId,
+      employeeId: employeeId,
+      responsibilities: responsibilities,
+    };
+    const response = await axios.post('project/projects-employee-responsibilities', data);
+    return response.data;
+  }
+  public async addResponsibilitiesToForm(myProjectEmployee: MyProjectEmployee) {
+    const { projectId, employeeId, responsibilities } = myProjectEmployee;
+    const data = {
+      projectId: projectId,
+      employeeId: employeeId,
+      responsibilities: responsibilities,
+    };
+    const response = await axios.post('project/projects-employee-responsibilities', data);
+    return response.data;
   }
 }
