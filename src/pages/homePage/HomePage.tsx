2ae1746f4281ed3092d601ab516ad978c30fd586
Jonas <jonasarmalis60@gmail.com>
2023-05-06 11:10:45 +0300

    removed commented code, pre-commit run

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,12 +1,12 @@
 import './HomePage.scss';
 
+import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 import Main from '../../components/main/Main';
 import { ROUTES } from '../../routes/routes';
-import { useEffect } from 'react';
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -32,7 +32,6 @@ const HomePage = () => {
     <main>
       <Header />
       <img src="https://logosandtypes.com/wp-content/uploads/2022/03/Cognizant.png" alt="" className="img"></img>
-      {/* <TabPanel index={0} value={0} /> */}
       <Main />
       <footer>
         <Footer />
